6e354125491b9237967381fb04a1a412504088d9
Stephen Fluin <stephen.fluin@gmail.com>
2022-11-21 10:20:38 -0600

    fix: tweak broken example-web image urls

--- examples-web/call-contract/components/layout/Navbar.tsx
+++ examples-web/call-contract/components/layout/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
         <header className="h-16 bg-white shadow-sm">
             <nav className="container flex items-center w-full h-full px-4 mx-auto">
                 <div>
-                    <img className="h-8" src="https://axelar.network/wp-content/uploads/2022/02/Axelar-Logo-Dark.svg" alt="axelar" />
+                    <img className="h-8" src="https://avatars.githubusercontent.com/u/67522309?s=200&v=4" alt="axelar" />
                 </div>
             </nav>
         </header>
