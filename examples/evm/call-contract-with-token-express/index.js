3cb1f3d6ce6f63c06fb497caab986c524057c760
npty <nptytn2@gmail.com>
2023-02-15 07:53:33 +0700

    chore: update deploy method

--- examples/evm/call-contract-with-token-express/index.js
+++ examples/evm/call-contract-with-token-express/index.js
@@ -6,11 +6,15 @@ const {
     constants: { AddressZero },
     ethers,
 } = require('ethers');
-const { deployUpgradable } = require('@axelar-network/axelar-gmp-sdk-solidity');
+const {
+    utils: { deployContract },
+} = require('@axelar-network/axelar-local-dev');
+// const { deployUpgradable } = require('@axelar-network/axelar-gmp-sdk-solidity');
 const DistributionExecutable = rootRequire(
     './artifacts/examples/evm/call-contract-with-token-express/DistributionExpressExecutable.sol/DistributionExpressExecutable.json',
 );
 const ExpressProxy = require('@axelar-network/axelar-gmp-sdk-solidity/artifacts/contracts/express/ExpressProxy.sol/ExpressProxy.json');
+// const ExpressProxy = require('@axelar-network/axelar-gmp-sdk-solidity/artifacts/contracts/express/');
 const Gateway = rootRequire(
     './artifacts/@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol/IAxelarGateway.json',
 );
@@ -20,23 +24,27 @@ async function deploy(chain, wallet) {
     console.log(`Deploying DistributionExecutable for ${chain.name}.`);
     const provider = getDefaultProvider(chain.rpc);
     chain.wallet = wallet.connect(provider);
-    console.log(`Deploying DistributionProxy for ${chain.name}.`);
-    chain.contract = await deployUpgradable(
-        chain.constAddressDeployer,
-        wallet,
-        DistributionExecutable,
-        ExpressProxy,
-        [chain.gateway, chain.gasService],
-        [chain.gateway, '0xfb72239394647e97894585D0D93Ca91f6C3852a4'],
-        '0x',
+    chain.contract = await deployContract(chain.wallet, DistributionExecutable, [chain.gateway, chain.gasService]);
+    console.log(`Deployed DistributionExecutable for ${chain.name} at ${chain.contract.address}.`);
+    chain.expressService = new Contract(
+        '0xfb72239394647e97894585D0D93Ca91f6C3852a4',
+        [
+            'function deployExpressProxy(bytes32, address, address, bytes calldata) public returns (address)',
+            'function deployedProxyAddress(bytes32,address) public view returns (address)',
+            'function isExpressProxy(address) public view returns (bool)',
+        ],
+        chain.wallet,
     );
-    console.log(`Deployed DistributionProxy for ${chain.name} at ${chain.contract.address}.`);
+    const salt = ethers.utils.formatBytes32String(parseInt(Math.random() * 10000000).toString());
+    await chain.expressService.deployExpressProxy(salt, chain.contract.address, wallet.address, '0x').then((tx) => tx.wait());
+    const proxyAddress = await chain.expressService.deployedProxyAddress(salt, chain.contract.address);
+    console.log(`Deployed Proxy for ${chain.name}`, proxyAddress);
     const gateway = new Contract(chain.gateway, Gateway.abi, chain.wallet);
     const usdcAddress = await gateway.tokenAddresses('aUSDC');
     chain.usdc = new Contract(usdcAddress, IERC20.abi, chain.wallet);
-    chain.proxy = new Contract(chain.contract.address, ExpressProxy.abi, chain.wallet);
-    await chain.proxy.deployRegistry();
-    console.log(`Deployed Registry for ${chain.name} at ${await chain.proxy.registry()}.`);
+    chain.proxy = new Contract(proxyAddress, ExpressProxy.abi, chain.wallet);
+    await chain.proxy.deployRegistry().then(tx => tx.wait());
+    // console.log(`Deployed Registry for ${chain.name} at ${await chain.proxy.registry()}.`);
 }
 
 async function execute(chains, wallet, options) {
@@ -46,6 +54,17 @@ async function execute(chains, wallet, options) {
     const destination = chains.find((chain) => chain.name === (args[1] || 'Fantom'));
     const amount = Math.floor(parseFloat(args[2])) * 1e6 || 10e6;
     const accounts = args.slice(3);
+
+    // check if the proxy is valid
+    console.log('source.proxy.address', source.proxy.address)
+    // console.log(source.expressService)
+    const registry = await source.proxy.registry();
+    console.log("registry", registry)
+    const validProxySrc = await source.expressService.isExpressProxy(source.proxy.address)
+    const validProxyDest = await destination.expressService.isExpressProxy(destination.proxy.address)
+    console.log('validProxySrc', validProxySrc)
+    console.log('validProxyDest', validProxyDest)
+    if(!validProxySrc || !validProxyDest) throw new Error('Invalid proxy address')
 
     if (accounts.length === 0) accounts.push(wallet.address);
 
@@ -66,7 +85,7 @@ async function execute(chains, wallet, options) {
     const approveTx = await source.usdc.approve(source.contract.address, amount);
     await approveTx.wait();
 
-    const sendTx = await source.contract.sendToMany(destination.name, destination.contract.address, accounts, 'aUSDC', amount, {
+    const sendTx = await source.contract.sendToMany(destination.name, destination.proxy.address, accounts, 'aUSDC', amount, {
         value: BigInt(Math.floor(gasLimit * gasPrice)),
     });
     await sendTx.wait();
@@ -75,11 +94,17 @@ async function execute(chains, wallet, options) {
     // express call
     const payload = ethers.utils.defaultAbiCoder.encode(['address[]'], [accounts]);
 
-    const approveDestTx = await destination.usdc.approve(destination.contract.address, amount);
+    const approveDestTx = await destination.usdc.approve(destination.proxy.address, amount);
     await approveDestTx.wait();
-    await destination.proxy.expressExecuteWithToken(source.name, source.contract.address, payload, 'aUSDC', amount);
+    console.log('approveDestTx', approveDestTx.hash);
+    const tx = await destination.proxy
+        .expressExecuteWithToken(source.name, source.proxy.address, payload, 'aUSDC', amount)
+        .then((tx) => tx.wait());
+
+    console.log('expressExecuteWithToken', tx.transactionHash);
 
     console.log('--- After ---');
+    await sleep(5000);
     await logAccountBalances();
 }
 
